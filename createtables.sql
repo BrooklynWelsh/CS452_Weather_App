8c10d7b1a7f7ee3bd7bf2f8f1e99f934b7c38f3b
BrooklynWelsh <welsh_bd@lynchburg.edu>
2020-09-09 14:46:34 -0400

    Added populatetables.sql to add test data to tables

--- createtables.sql
+++ createtables.sql
@@ -1,53 +1,89 @@
+-- Code for dropping tables instead of the entire DB, kept just in case needed
+DO $$ DECLARE
+    r RECORD;
+BEGIN
+    -- This loop removes all tables in the current DB
+    -- if the schema you operate on is not "current", you will want to
+    -- replace current_schema() in query with 'schematodeletetablesfrom'
+    -- *and* update the generate 'DROP...' accordingly.
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+    END LOOP;
+END $$;
+
+--DROP DATABASE IF EXISTS weather_app;
+--CREATE DATABASE weather_app WITH ENCODING UTF8 OWNER pi;
+--SELECT DATABASE weather_app;
 
 CREATE TABLE IF NOT EXISTS stations (
-	stationID int,
+	stationID SERIAL,
 	name varchar(100),
 	latitude float NOT NULL,
 	longitude float NOT NULL,
 	altitude float NOT NULL,
-	CONSTRAINT stations_pk PRIMARY KEY (stationID)
+	CONSTRAINT stations_pk PRIMARY KEY (stationID),
+	CONSTRAINT unique_attributes UNIQUE (name, latitude, longitude)
 );
 
 CREATE TABLE IF NOT EXISTS measurements_current (
 	stationID int,
-	fahrenheit real NOT NULL,
-	celsius real  NOT NULL,
-	wind_speed real NOT NULL,
-	wind_gust real NOT NULL,
-	pressure real NOT NULL,
-	humidity real NOT NULL,
-	rain real NOT NULL,
-	cloud varchar(3) NOT NULL,
-	heat_index real NOT NULL,
+	fahrenheit real,
+	celsius real,
+	wind_speed real,
+	wind_gust real,
+	pressure real,
+	humidity real,
+	rain real,
+	cloud varchar(3),
+	heat_index real,
 	CONSTRAINT current_pk PRIMARY KEY (stationID)
 );
 
 CREATE TABLE IF NOT EXISTS measurements_historical (
-	time_stamp timestamp,
+        measurement_date  date,
 	stationID int,
-	fahrenheit real NOT NULL,
-	celsius real NOT NULL,
-	wind_speed real NOT NULL,
-	wind_gust real NOT NULL,
-	pressure real NOT NULL,
-	humidity real NOT NULL,
-	rain real NOT NULL,
-	cloud varchar(3) NOT NULL,
-	heat_index real NOT NULL,
-	CONSTRAINT historical_pk PRIMARY KEY (time_stamp, stationID)
+	fahrenheit_high real,
+	fahrenheit_low real,
+	celsius_high real,
+	celsius_low real,
+	wind_speed real,
+	wind_gust real,
+	pressure real,
+	humidity real,
+	rain real,
+	cloud varchar(3),
+	heat_index real,
+	CONSTRAINT historical_pk PRIMARY KEY (measurement_date, stationID)
 );
 
-CREATE TABLE IF NOT EXISTS measurements_forecast (
-	time_stamp timestamp,
+CREATE TABLE IF NOT EXISTS forecast_hourly (
+	hour time,
 	stationID int,
-	fahrenheit real NOT NULL,
-	celsius real NOT NULL,
-	wind_speed real NOT NULL,
-	wind_gust real NOT NULL,
-	pressure real NOT NULL,
-	humidity real NOT NULL,
-	rain real NOT NULL,
-	cloud varchar(3) NOT NULL,
-	heatindex real NOT NULL,
-	CONSTRAINT forecast_pk PRIMARY KEY (time_stamp, stationID)
+	fahrenheit real,
+	celsius real,
+	wind_speed real,
+	wind_gust real,
+	pressure real,
+	humidity real,
+	rain_probability real,
+	cloud varchar(3),
+	heatindex real,
+	CONSTRAINT forecast_hourly_pk PRIMARY KEY (hour, stationID)
+);
+
+CREATE TABLE IF NOT EXISTS forecast_daily (
+	day date,
+	stationID int,
+	fahrenheit_high real,
+	fahrenheit_low real,
+	celsius_high real,
+	celsius_low real,
+	wind_speed real,
+	wind_gust real,
+	pressure real,
+	humidity real,
+	rain_probability real,
+	cloud varchar(3),
+	heatindex real,
+	CONSTRAINT forecast_daily_pk PRIMARY KEY (day, stationID)
 );
